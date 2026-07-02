2a69b4058c976a3d225a91b92809f927d0b8caac
Vaishnavi Deshmukh <b18_2022_ecse@yes.edu.in>
2026-07-02 23:17:59 +0530

    Fix: add multer upload export to cloudinary config

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
 const { uploadResume } = require('../controllers/userController');
 const cloudinary = require('../config/cloudinary');
-const upload = cloudinary.upload;
+const { upload } = require('../config/cloudinary');
 
 router.put('/profile',  protect, updateProfile);
 router.post('/resume',  protect, authorize('candidate'), upload.single('resume'), uploadResume);
